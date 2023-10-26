9464bc1a5e310e4b677051592d1060bc150d35a9
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-10-26 03:37:37 +0000

    update README.md

--- ci.ts
+++ ci.ts
@@ -1,7 +1,7 @@
 import {
   preview,
   up,
-} from "https://pkg.fluentci.io/pulumi_pipeline@v0.2.1/mod.ts";
+} from "https://pkg.fluentci.io/pulumi_pipeline@v0.2.0/mod.ts";
 
 await preview();
 await up();
