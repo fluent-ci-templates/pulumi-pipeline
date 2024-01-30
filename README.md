5e921df7c3415bc7f1ffe20e743df95c4c4aceb5
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-01-30 17:41:34 +0000

    fix issue with dagger connect
    ci: fix zenith workflow
    
    ci: fix zenith workflow

--- README.md
+++ README.md
@@ -76,7 +76,7 @@ up(
 You can also use this pipeline programmatically:
 
 ```ts
-import { preview, up } from "https://pkg.fluentci.io/pulumi_pipeline@v0.3.0/mod.ts";
+import { preview, up } from "https://pkg.fluentci.io/pulumi_pipeline@v0.3.1/mod.ts";
 
 await preview(
   ".", 
--- src/dagger/lib.ts
+++ src/dagger/lib.ts
@@ -2,19 +2,23 @@ import { Container } from "../../deps.ts";
 import { Directory, DirectoryID, Secret, SecretID } from "../../deps.ts";
 import { Client } from "../../sdk/client.gen.ts";
 
-export const getDirectory = (
+export const getDirectory = async (
   client: Client,
   src: string | Directory | undefined = "."
 ) => {
-  if (typeof src === "string" && src.startsWith("core.Directory")) {
-    return client.directory({
-      id: src as DirectoryID,
-    });
+  if (typeof src === "string") {
+    try {
+      const directory = client.loadDirectoryFromID(src as DirectoryID);
+      await directory.id();
+      return directory;
+    } catch (_) {
+      return client.host().directory(src);
+    }
   }
   return src instanceof Directory ? src : client.host().directory(src);
 };
 
-export const getPulumiAccessToken = (
+export const getPulumiAccessToken = async (
   client: Client,
   token?: string | Secret
 ) => {
@@ -25,10 +29,13 @@ export const getPulumiAccessToken = (
     );
   }
   if (token && typeof token === "string") {
-    if (token.startsWith("core.Secret")) {
-      return client.loadSecretFromID(token as SecretID);
+    try {
+      const secret = client.loadSecretFromID(token as SecretID);
+      await secret.id();
+      return secret;
+    } catch (_) {
+      return client.setSecret("PULUMI_ACCESS_TOKEN", token);
     }
-    return client.setSecret("PULUMI_ACCESS_TOKEN", token);
   }
   if (token && token instanceof Secret) {
     return token;
